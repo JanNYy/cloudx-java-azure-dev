339c5ce26b8062084ae91fd16017cbb7675ea7a8
Hanna Oliinyk <hanna_oliinyk@epam.com>
2025-10-24 19:00:37 +0300

    Updated ProductManagementService for the Module 5: added custom logging and exception

--- petstore/petstoreapp/src/main/java/com/chtrembl/petstoreapp/service/ProductManagementService.java
+++ petstore/petstoreapp/src/main/java/com/chtrembl/petstoreapp/service/ProductManagementService.java
@@ -31,59 +31,75 @@ public class ProductManagementService {
     private final ProductServiceClient productServiceClient;
 
     public Collection<Product> getProductsByCategory(String category, List<Tag> tags) {
-        List<Product> products;
-
-        MDC.put(OPERATION, "getProducts");
-        MDC.put(CATEGORY, category);
-
-        String requestId = MDC.get(REQUEST_ID);
-        String traceId = MDC.get(TRACE_ID);
-
-        log.info("Starting product retrieval operation [RequestID: {}, TraceID: {}, Category: {}]",
-                requestId, traceId, category);
-
         try {
-            this.sessionUser.getTelemetryClient().trackEvent(
-                    String.format("PetStoreApp user %s is requesting to retrieve products from the ProductService",
-                            this.sessionUser.getName()),
-                    this.sessionUser.getCustomEventProperties(), null);
-
-            products = productServiceClient.getProductsByStatus(AVAILABLE.getValue());
-            this.sessionUser.setProducts(products);
-
-            if (tags.stream().anyMatch(t -> t.getName().equals("large"))) {
-                products = products.stream()
-                        .filter(product -> category.equals(product.getCategory().getName())
-                                && product.getTags().toString().contains("large"))
-                        .toList();
-            } else {
-                products = products.stream()
-                        .filter(product -> category.equals(product.getCategory().getName())
-                                && product.getTags().toString().contains("small"))
-                        .toList();
-            }
-
-            log.info("Successfully retrieved {} products for category {} with tags {} [RequestID: {}, TraceID: {}]",
-                    products.size(), category, tags, requestId, traceId);
-
-            return products;
-        } catch (FeignException fe) {
-            log.error("Feign error retrieving products [RequestID: {}, TraceID: {}, Category: {}, HTTP: {}, Message: {}]",
-                    requestId, traceId, category, fe.status(), fe.getMessage(), fe);
-
-            this.sessionUser.getTelemetryClient().trackException(fe);
-            this.sessionUser.getTelemetryClient().trackEvent(
-                    String.format("PetStoreApp %s received Feign error %s (HTTP %d), container host: %s",
-                            this.sessionUser.getName(),
-                            fe.getMessage(),
-                            fe.status(),
-                            this.containerEnvironment.getContainerHostName())
-            );
-            log.error("Failed to retrieve products from ProductService via Feign client", fe);
-            throw new ProductServiceException("Unable to retrieve products from product service", fe);
-        } finally {
-            MDC.remove(OPERATION);
-            MDC.remove(CATEGORY);
+            throw new Exception("Cannot move further");
+        } catch (Exception exc) {
+            throw new RuntimeException(exc);
         }
+
+//        Comment of all the lines for the task Introduce an error in com.chtrembl.petstoreapp.service.ProductManagementService#getProductsByCategory() by adding the line:
+
+
+//        List<Product> products;
+//
+//        MDC.put(OPERATION, "getProducts");
+//        MDC.put(CATEGORY, category);
+//
+//        String requestId = MDC.get(REQUEST_ID);
+//        String traceId = MDC.get(TRACE_ID);
+//
+//        log.info("Starting product retrieval operation [RequestID: {}, TraceID: {}, Category: {}]",
+//                requestId, traceId, category);
+//
+//        try {
+//            this.sessionUser.getTelemetryClient().trackEvent(
+//                    String.format("PetStoreApp user %s is requesting to retrieve products from the ProductService",
+//                            this.sessionUser.getName()),
+//                    this.sessionUser.getCustomEventProperties(), null);
+//
+//            this.sessionUser.getTelemetryClient().trackEvent(
+//                    String.format("PetStoreApp (task add a custom event or log): username %s, session id %s",
+//                            this.sessionUser.getName(),
+//                            this.sessionUser.getSessionId()));
+//
+//            products = productServiceClient.getProductsByStatus(AVAILABLE.getValue());
+//            this.sessionUser.setProducts(products);
+//
+//            if (tags.stream().anyMatch(t -> t.getName().equals("large"))) {
+//                products = products.stream()
+//                        .filter(product -> category.equals(product.getCategory().getName())
+//                                && product.getTags().toString().contains("large"))
+//                        .toList();
+//            } else {
+//                products = products.stream()
+//                        .filter(product -> category.equals(product.getCategory().getName())
+//                                && product.getTags().toString().contains("small"))
+//                        .toList();
+//            }
+//
+//            log.info("Successfully retrieved {} products for category {} with tags {} [RequestID: {}, TraceID: {}]",
+//                    products.size(), category, tags, requestId, traceId);
+//
+//            this.sessionUser.getTelemetryClient().trackMetric("ProductsReturnedCount", products.size());
+//
+//            return products;
+//        } catch (FeignException fe) {
+//            log.error("Feign error retrieving products [RequestID: {}, TraceID: {}, Category: {}, HTTP: {}, Message: {}]",
+//                    requestId, traceId, category, fe.status(), fe.getMessage(), fe);
+//
+//            this.sessionUser.getTelemetryClient().trackException(fe);
+//            this.sessionUser.getTelemetryClient().trackEvent(
+//                    String.format("PetStoreApp %s received Feign error %s (HTTP %d), container host: %s",
+//                            this.sessionUser.getName(),
+//                            fe.getMessage(),
+//                            fe.status(),
+//                            this.containerEnvironment.getContainerHostName())
+//            );
+//            log.error("Failed to retrieve products from ProductService via Feign client", fe);
+//            throw new ProductServiceException("Unable to retrieve products from product service", fe);
+//        } finally {
+//            MDC.remove(OPERATION);
+//            MDC.remove(CATEGORY);
+//        }
     }
 }
